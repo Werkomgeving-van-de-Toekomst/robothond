1221967f3ea48e21152285b1b3e94a7427f3b231
Marc Minnee <marc.minnee@gmail.com>
2025-12-08 11:32:21 +0100

    Export FlowExecutor in package __init__

--- src/unitree_go2/__init__.py
+++ src/unitree_go2/__init__.py
@@ -7,7 +7,17 @@ Een Python wrapper voor de Unitree Go2 EDU robot API.
 from .robot import Go2Robot
 from .exceptions import Go2ConnectionError, Go2CommandError
 from .config import load_config
+from .flow_executor import FlowExecutor, FlowAction, ActionType, create_welcome_flow
 
 __version__ = "0.1.0"
-__all__ = ["Go2Robot", "Go2ConnectionError", "Go2CommandError", "load_config"]
+__all__ = [
+    "Go2Robot",
+    "Go2ConnectionError",
+    "Go2CommandError",
+    "load_config",
+    "FlowExecutor",
+    "FlowAction",
+    "ActionType",
+    "create_welcome_flow"
+]
 
